2ec197240f76015d49003a590f3c4caceea141d8
Nivo Tech <nivotech.dev@gmail.com>
2026-02-01 22:08:16 +0000

    diminua 10px da logo (_for element img_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,14 +4,14 @@ import { cn } from "@/lib/utils";
 
 export default function Logo({ className, variant = 'default' }: { className?: string, variant?: 'default' | 'inverted' }) {
   return (
-    <Link href="/" className={cn("relative block w-36 md:w-52 h-20", className)}>
+    <Link href="/" className={cn("relative block w-28 md:w-40 h-16", className)}>
       <Image
         src="https://whfdrrdozhyavyflgaxo.supabase.co/storage/v1/object/public/marcosguerra/Design_sem_nome__3_-removebg-preview.png"
         alt="Dr. Marcos Guerra Logo"
         fill
         className="object-contain"
         style={variant === 'inverted' ? { filter: 'brightness(0) invert(1)' } : {}}
-        sizes="(max-width: 768px) 144px, 208px"
+        sizes="(max-width: 768px) 112px, 160px"
         priority
       />
     </Link>
