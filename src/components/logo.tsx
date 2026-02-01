73f84eb8a8b4dc53283639637abc2f73f6b1ac5b
Nivo Tech <nivotech.dev@gmail.com>
2026-02-01 01:27:31 +0000

    troque esta logo para esta outra img: 'https://whfdrrdozhyavyflgaxo.supa

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,7 +6,7 @@ export default function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("relative block w-60 h-16", className)}>
       <Image
-        src="https://whfdrrdozhyavyflgaxo.supabase.co/storage/v1/object/public/marcosguerra/Clinica_ortodontia.webp"
+        src="https://whfdrrdozhyavyflgaxo.supabase.co/storage/v1/object/public/marcosguerra/Clinica_ortodontia.jpg"
         alt="Dr. Marcos Guerra Logo"
         fill
         className="object-contain"
