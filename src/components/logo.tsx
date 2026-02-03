1f768f5bca26a06fada139cc5c3cad1fa3945f3b
Nivo Tech <nivotech.dev@gmail.com>
2026-02-03 18:14:47 +0000

    Make the following changes:
    Find this element:
    
    Relevant files:

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,4 +1,3 @@
-
 import Link from "next/link";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
@@ -7,7 +6,7 @@ export default function Logo({ className }: { className?: string, variant?: 'def
   return (
     <Link href="/" className={cn("relative block", className)}>
       <Image
-        src="https://whfdrrdozhyavyflgaxo.supabase.co/storage/v1/object/public/marcosguerra/Design_sem_nome__3_-removebg-preview.png"
+        src="https://whfdrrdozhyavyflgaxo.supabase.co/storage/v1/object/public/marcosguerra/Whisk_3a7f83b3ad665b48c774b64806b30ac7dr-removebg-preview.png"
         alt="Dr. Marcos Guerra Logo"
         fill
         className="object-contain"
